15891060d293323b4a0eef3d8d760a7d290600f4
Vincent Schiller <vincent.schiller@gmail.com>
2021-12-07 22:04:54 +0100

    Update pure_pursuit.py

--- group1_ws/src/group1_roslab/scripts/pure_pursuit.py
+++ group1_ws/src/group1_roslab/scripts/pure_pursuit.py
@@ -3,17 +3,21 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import LaserScan
 
-# TODO: import ROS msg types and libraries
+# import ROS msg types and libraries
+import os
+import csv
 from ackermann_msgs.msg import AckermannDriveStamped
 from tf.transformations import euler_from_quaternion
 import numpy as np
 
+L = 0.2
+index_last_waypoint = 300
+
 class PurePursuit(object):
-    global L = 0.2
-    global index_last_waypoint = 0
+    
     # Import waypoints.csv into a list (path_points)
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, ’../waypoints/levine-waypoints.csv’)
+    filename = os.path.join(dirname, '../waypoints/levine-waypoints.csv')
     with open(filename) as f:
         path_points = [tuple(line) for line in csv.reader(f)]
     # Turn path_points into a list of floats to eliminate the need for casts in the code below.
@@ -27,29 +31,55 @@ class PurePursuit(object):
         #Topic for real world
         #self.pose_sub = rospy.Subscriber('/pf/inferred_pose', PoseStamped, self.pose_callback)
         self.drive_pub = rospy.Publisher('vesc/high_level/ackermann_cmd_mux/input/nav_0', AckermannDriveStamped, queue_size = 10)
+        
 
     def pose_callback(self, pose_msg):
-        eulerAngles = euler_from_quaternion(pose_msg.pose.orientation)
-    
-        # TODO: find the current waypoint to track using methods mentioned in lecture
-        for i in range(index_last_waypoint,path_points.size - 1):
-            nearest = np.inf
-            nearest_dist = np.inf
-            point_dist = abs(# Abstand car pos zu Ursprung -np.sqrt(path_points[i][0]**2 + path_points[i][1]**2))
-            if point_dist > L and point_dist < nearest_dist:
-                nearest = path_points[i]
-                nearest_dist = point_dist
+        global L
+        global index_last_waypoint
+        
+        eulerAngles = euler_from_quaternion((pose_msg.pose.orientation.x,pose_msg.pose.orientation.y,pose_msg.pose.orientation.z,pose_msg.pose.orientation.w))
+        
+        x_pos = pose_msg.pose.position.x
+        y_pos = pose_msg.pose.position.y
+        
+        nearest_index = index_last_waypoint
+        nearest_dist = np.inf
+        
+        # Find the current waypoint to track using methods mentioned in lecture
+        if index_last_waypoint >= len(self.path_points) - 1:
+           print ('goal!')
+        else:
+            for i in range(index_last_waypoint,len(self.path_points) - 1):
+            
+                point_dist = abs(np.sqrt(x_pos**2 + y_pos**2) -np.sqrt(self.path_points[i][0]**2 + self.path_points[i][1]**2))
+                if point_dist > L and point_dist < nearest_dist:
+                    nearest_index = i
+                    nearest_dist = point_dist
+        
+            index_last_waypoint = nearest_index
+        
         
 
+        # transform goal point to vehicle frame of reference
+        x = self.path_points[nearest_index][0] - x_pos
+        y = self.path_points[nearest_index][1] - y_pos
+        x_dist = x*np.cos(eulerAngles[2])-y*np.sin(eulerAngles[2])   
+        y_dist = y*np.cos(eulerAngles[2])+x*np.sin(eulerAngles[2]) 
         
+       
 
-
-        # TODO: transform goal point to vehicle frame of reference
-
-        # TODO: calculate curvature/steering angle
-
-        # TODO: publish drive message, don't forget to limit the steering angle between -0.4189 and 0.4189 radians
-
+        # calculate curvature/steering angle
+        curvature = 2*y_dist/(L**2)
+        print(curvature)
+        # publish drive message, don't forget to limit the steering angle between -0.4189 and 0.4189 radians
+        if abs(curvature) > 0.4189:
+            curvature = 0.4189 * np.sign(curvature)
+        
+        drive_msg = AckermannDriveStamped()
+        drive_msg.header.stamp = rospy.Time.now()
+        drive_msg.drive.steering_angle = curvature
+        drive_msg.drive.speed = 1
+        self.drive_pub.publish(drive_msg)
 
 def main():
     rospy.init_node('pure_pursuit_node')
